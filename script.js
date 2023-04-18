120f0410334a72c3907585fd66eae1ef84c5e2f2
DCoder18 <devikasujith12@gmail.com>
2023-04-18 11:52:09 +0400

    Update card display function

--- script.js
+++ script.js
@@ -1,103 +1,25 @@
-// $(".name").change(function() {
-//   alert("This workd.")
-//   $(".card-front--name").text($(".name").val())
-// })
-
 $(document).ready( function () {
   const displayElArray = []
   const inputElArray = []
-  // let k = ''
 
-
-
+  // Loop through display classes and save the elements to the display array
   $(".card-display").each(function(i, elDisplay) {
     displayElArray.push(elDisplay)
   });
 
+  //Switch first two elements to match the Input Array
+  displayElArray[0] = displayElArray.splice(1, 1, displayElArray[0])[0]
+
+  // Loop through input classes and save the elements to the input array
   $(".card-input").each(function(j, elInputs) {
-    // inputElArray.push(elInputs)
-    elInputs.addEventListener('change', (event) => {
-      console.log(elInputs[j]);
-      // displayElArray[j].text()
+    const inputElID = `#${elInputs.id}`
+    inputElArray.push(elInputs)
+
+    // Then replace display element text with input data
+    $(inputElID).change(function() {
+      $(displayElArray[j]).text($(inputElID).val())
     })
-    // console.log(elInputs);
-
-    // console.log(elInputs);
-    // elInputs[i].change(function() {
-    //   displayElArray[j].text(inputElArray[i].val())
-    // })
   });
-
-  // $.each(inputElArray, function(i) {
-  //   const display_el = displayElArray[i];
-  //   const input_el = inputElArray[i];
-  //   console.log(display_el);
-  //   console.log(input_el);
-    
-  //   input_el.addEventListener('change', (event) => {
-  //     console.log($(".name").val());
-  //     display_el.text(input_el.val())
-  //   })
-  // })
-
-
-  // $.each(inputElArray, function(k, inputs) {
-    
-    // console.log(inputElArray[k]);
-    // inputElArray[k].change(function() {
-    // inputElArray[k].change(function() {
-      // console.log('workd');
-      // displayElArray[l].text(inputs.val())
-    // })
-    // inputs[k].change(function() {
-    //   displayElArray[0].text(inputs.val())
-    // })
-    // console.log(inputs, k, inputElArray);
-    // console.log(inputElArray[k]);
-    // inputElArray[i].change(function() {
-    //   displayElArray[j].text(inputElArray[i].val())
-    // })
-  // })
-
-
-  // console.log(displayElArray);
-  // console.log(inputElArray);
-
 });
 
 
-// function getElements() {
-//   $('.card-display').each( fuction (i)) {
-//     console.log('')
-//   }
-// }
-
-
-
-// const elCardNo = document.querySelector('.card-no')
-// const elExpDateMonth = document.querySelector('.exp-date--mm')
-// const elExpDateYear = document.querySelector('.exp-date--yy')
-// const elCVC = document.querySelector('.cvc')
-
-// const elNameDisplay = document.querySelector('.card-front--name')
-// const elNumberDisplay = document.querySelector('.card-front--no')
-// const elExpiryMonthDisplay = document.querySelector('.card-front--exp-month')
-// const elExpiryYearDisplay = document.querySelector('.card-front--exp-year')
-// const elCVCDisplay = document.querySelector('.card-back--cvc')
-
-// const inputElArray = [elCardHolderName, elCardNo, elExpDateMonth, elExpDateYear, elCVC]
-// const displayElArray = [elNameDisplay, elNumberDisplay, elExpiryMonthDisplay, elExpiryYearDisplay, elCVCDisplay]
-// const q = ['.name', '.card-no', '.exp-date--mm', '.exp-date--yy', '.cvc']
-
-// function updateCardDisplay() {
-//   inputElArray.forEach((input_el, i) => {
-//     const display_el = displayElArray[i];
-//     const query = q[i];
-
-//     input_el.addEventListener('change', (event) => {
-//       display_el.textContent = document.querySelector(query).value;
-//     })
-//   })
-// }
-
-// updateCardDisplay()
