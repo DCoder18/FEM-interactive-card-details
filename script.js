a0b94b029208032a9c028d927c889952d7536f9d
DCoder18 <devikasujith12@gmail.com>
2023-04-18 13:05:56 +0400

    Validate inputs fnction (WIP)

--- script.js
+++ script.js
@@ -2,24 +2,49 @@ $(document).ready( function () {
   const displayElArray = []
   const inputElArray = []
 
-  // Loop through display classes and save the elements to the display array
+  /* 
+  - Loop through display classes
+  - Save the elements to the display array
+  - Reposition items in display array
+  */
   $(".card-display").each(function(i, elDisplay) {
     displayElArray.push(elDisplay)
-  });
+  })
 
-  //Switch first two elements to match the Input Array
+
+  // Switch first two elements to match the Input Array 
   displayElArray[0] = displayElArray.splice(1, 1, displayElArray[0])[0]
 
-  // Loop through input classes and save the elements to the input array
+
+  /*
+  - Loop through input classes
+  - Save the elements to the input array
+  - Replace display element text with input data
+  */
   $(".card-input").each(function(j, elInputs) {
     const inputElID = `#${elInputs.id}`
     inputElArray.push(elInputs)
 
-    // Then replace display element text with input data
-    $(inputElID).change(function() {
+    validateInput(this)
+
+    $(inputElID).keyup(function() {
       $(displayElArray[j]).text($(inputElID).val())
     })
-  });
-});
+  })
+
+  
+
+
+  // Helper Functions
+  function validateInput(input) {
+    const txtblank = "<p>Can't be blank</p>"
+    if(! $(`#${input.id}`).val())
+      $(`#${input.id}`).after(txtblank)
+      // alert('tis empty!')
+    }
+})
+
+
+
 
 
